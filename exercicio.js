d093e3920939b51187a54dedcbe63badf32c4bcc
Danilo Jesús <142642955+DaniloJesus21@users.noreply.github.com>
2025-04-22 14:40:51 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+console.log(multiplicar(5, 2));
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(saudacao('Danilo'));
---
+++ exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicar(a:number, b:number){
+  return a * b
+}
+
+console.log(multiplicar(5,2))
+
+function saudacao(nome:string) {
+  return `Olá ${nome}`
+}
+
+console.log(saudacao('Danilo'))
